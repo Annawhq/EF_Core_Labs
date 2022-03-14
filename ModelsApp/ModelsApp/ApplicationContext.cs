37066f699373ffdec46d542b0a4fb6b3fe6a8b9e
Anna Shkolina <annashkolina01@gmail.com>
2022-03-14 14:36:38 +0300

    developing models

---
+++ ModelsApp/ModelsApp/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsApp
+{
+    public class ApplicationContext : DbContext
+ {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=efbasicsappdb; Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().HasKey(u => u.Id);
+            modelBuilder.Entity<Company>().HasData(
+            new Company[]
+            {
+                new Company { Id=1, Name="Microsoft"},
+                new Company { Id=2, Name="Apple"},
+                new Company { Id=3, Name="Google"}
+            });
+        }
+    }
+}
---
+++ ModelsApp/ModelsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ModelsApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // создаем два объекта Company
+                Product product1 = new Product { Name = "Product1", Price = 200 };
+                Product product2 = new Product { Name = "Product2", Price = 100 };
+                // добавляем их в БД
+                db.Products.Add(product1);
+                db.Products.Add(product2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Products.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Product u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Price}");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
