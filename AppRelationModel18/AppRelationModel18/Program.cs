22714c6e88a5aaae26d7db495ad2a477297e06d0
Anna Shkolina <annashkolina01@gmail.com>
2022-04-07 20:38:48 +0300

    LINQ to Entities Queries

---
+++ AppRelationModel18/AppRelationModel18/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace AppRelationModel18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // пересоздаем базу данных
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                Company microsoft = new Company { Name = "Microsoft" };
+                Company google = new Company { Name = "Google" };
+                db.Companies.AddRange(microsoft, google);
+                User tom = new User { Name = "Tom", Age = 36, Company = microsoft };
+                User bob = new User { Name = "Bob", Age = 39, Company = google };
+                User alice = new User { Name = "Alice", Age = 28, Company = microsoft };
+                User kate = new User { Name = "Kate", Age = 25, Company = google };
+                db.Users.AddRange(tom, bob, alice, kate);
+                db.SaveChanges();
+            }
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.OrderBy(u => u.Age).ThenBy(u => u.Company.Name);
+                foreach (User user in users)
+                    Console.WriteLine($"{user.Id}.{user.Name} ({user.Age})");
+            }
+
+        }
+    }
+}
